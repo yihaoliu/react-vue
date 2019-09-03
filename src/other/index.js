8cbcded3da88030dee7e192aa55782f7ba96f8a0
mac <15711367521@163.com>
2019-09-03 08:45:33 +0800

    ok

---
+++ src/other/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+// import App from './App.vue'
+console.log(Vue,"kkkkk")
+Vue.config.productionTip = false
+
+console.log(new Vue({
+  el: '#root',
+  // router,
+  data: {
+    message: 'Hello Vue!'
+  }
+}))
+
